192e1dcd5f9819e11543736cc0ff5bafc0da6d9f
Rohmadz22 <desiformagang22.gmail.com>
2022-03-29 07:08:23 +0700

    commit kedua

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,19 @@
-<h1>Charity</h1>
+<script>
+	import CharityList from './CharityList.svelte';
+
+let title = "Charity";
+setTimeout(function() {
+	title = "Donation";	
+},2000);
+
+</script>
+
+<h1>{title}</h1>
+<CharityList/>
+
+<style>
+h1 {
+	font-size: 3em;
+	color: #e2071c;
+}	
+</style>
---
+++ src/CharityList.svelte
@@ -0,0 +1,5 @@
+<h2>Daftar Charity</h2>
+<ul>
+    <i>Charity 1</i>
+    <i>Charity 2</i>
+</ul>
